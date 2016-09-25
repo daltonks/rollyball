8de268795f2c18cfac1276f93dc822be5f1b0528
daltonks <dalton_spillman@yahoo.com>
2016-09-25 15:56:16 -0500

    Removed raycasts for jumping/walljumping and am instead allowing a window of time after collisions to jump/walljump

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -7,60 +7,37 @@ public class PlayerController : MonoBehaviour
     public float Speed;
     public float MaxVelocitySquared;
     public float JumpSpeed;
-    public float JumpInAirMaxTime;
-    public float MinTimeBetweenJumps;
+    public float TimeBetweenJumps;
+    public float JumpRaycastDistance;
+    public float JumpAfterCollideMax;
 
     private float FallingTime = 0;
-    private bool IsFalling = false;
+    private float JumpAfterCollideCountdown = 0;
+    private float WallJumpAfterCollideCountdown = 0;
     private bool IsInJumpMode = false;
     private bool HasWallJumped = false;
     private float Radius;
     private Rigidbody RigidBody;
-    private Vector3[] GroundRaycastVectors, WallRaycastVectors;
-    private float JumpAccum;
+    private float TimeBetweenJumpsAccum;
     public CameraController Camera { get; set; }
     public MasterController Master { get; set; }
 
     void Start ()
     {
-        JumpAccum = MinTimeBetweenJumps;
+        TimeBetweenJumpsAccum = TimeBetweenJumps;
         Radius = GetComponent<MeshRenderer>().bounds.extents.x;
-
         RigidBody = GetComponent<Rigidbody>();
-        //Coordinates created by making a UV sphere in Blender
-        GroundRaycastVectors = new Vector3[]
-        {
-            new Vector3(0, -1, 0),
-            new Vector3(0.11126f, -0.86603f, 0.48746f),
-            new Vector3(0.45048f, -0.86603f, 0.21694f),
-            new Vector3(0.45048f, -0.86603f, -0.21694f),
-            new Vector3(0.11126f, -0.86603f, -0.48746f),
-            new Vector3(-0.31174f, -0.86603f, -0.39092f),
-            new Vector3(-0.5f, -0.86603f, 0.0f),
-            new Vector3(-0.31175f, -0.86603f, 0.39092f),
-            new Vector3(0.19271f, -0.5f, 0.84431f),
-            new Vector3(0.78026f, -0.5f, 0.37575f),
-            new Vector3(0.78026f, -0.5f, -0.37575f),
-            new Vector3(0.19271f, -0.5f, -0.84431f),
-            new Vector3(-0.53996f, -0.5f, -0.67709f),
-            new Vector3(-0.86603f, -0.5f, -0.0f),
-            new Vector3(-0.53996f, -0.5f, 0.67709f)
-        };
-        WallRaycastVectors = new Vector3[]
-        {
-            new Vector3(0.22252f, -0.0f, 0.97493f),
-            new Vector3(0.90097f, -0.0f, 0.43388f),
-            new Vector3(0.90097f, -0.0f, -0.43388f),
-            new Vector3(0.22252f, -0.0f, -0.97493f),
-            new Vector3(-0.62349f, -0.0f, -0.78183f),
-            new Vector3(-1.0f, -0.0f, -0.0f),
-            new Vector3(-0.62349f, -0.0f, 0.78183f)
-        };
     }
 	
 	void FixedUpdate()
     {
-        if(transform.position.y <= Master.YDeath)
+        if (RigidBody.IsSleeping())
+            RigidBody.WakeUp();
+
+        JumpAfterCollideCountdown -= Time.fixedDeltaTime;
+        WallJumpAfterCollideCountdown -= Time.fixedDeltaTime;
+
+        if (transform.position.y <= Master.YDeath)
         {
             RigidBody.velocity = Vector3.zero;
             RigidBody.angularVelocity = Vector3.zero;
@@ -78,7 +55,6 @@ public class PlayerController : MonoBehaviour
         {
             fromCamera = Vector3.forward;
         }
-
         float tickSpeed = Speed;
         if(  (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
           && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
@@ -103,64 +79,48 @@ public class PlayerController : MonoBehaviour
             AddMovementForce(Quaternion.Euler(0, 90, 0), fromCamera, tickSpeed);
         }
         
-        JumpAccum += Time.fixedDeltaTime;
-        if(IsFalling)
+        TimeBetweenJumpsAccum += Time.fixedDeltaTime;
+
+        if(IsInJumpMode)
         {
-            if(IsInJumpMode)
+            FallingTime += Time.fixedDeltaTime;
+            if (Input.GetKey(KeyCode.Space))
             {
-                FallingTime += Time.fixedDeltaTime;
-                if (Input.GetKey(KeyCode.Space))
+                if (FallingTime >= TimeBetweenJumps)
                 {
-                    if(FallingTime >= JumpInAirMaxTime)
-                    {
-                        IsInJumpMode = false;
-                    }
-                    else
-                    {
-                        AddJumpForce();
-                    }
+                    IsInJumpMode = false;
                 }
                 else
                 {
-                    IsInJumpMode = false;
+                    AddJumpForce();
                 }
             }
             else
             {
-                if (!HasWallJumped && Input.GetKey(KeyCode.Space) && JumpAccum >= MinTimeBetweenJumps)
-                {
-                    Debug.Log("Try wall jump");
-                    foreach (Vector3 vec in WallRaycastVectors)
-                    {
-                        if (Physics.Raycast(transform.position, vec, Radius + .05f))
-                        {
-                            Debug.Log("Wall jump!");
-                            AddJumpForce();
-                            JumpAccum = 0;
-                            FallingTime = 0;
-                            IsFalling = true;
-                            IsInJumpMode = true;
-                            HasWallJumped = true;
-                            break;
-                        }
-                    }
-                }
+                IsInJumpMode = false;
             }
         }
-        else if (Input.GetKey(KeyCode.Space) && JumpAccum >= MinTimeBetweenJumps)
+        else if(Input.GetKey(KeyCode.Space) && TimeBetweenJumpsAccum >= TimeBetweenJumps)
         {
-            foreach(Vector3 vec in GroundRaycastVectors)
+            if (JumpAfterCollideCountdown > 0)
             {
-                if(Physics.Raycast(transform.position, vec, Radius + .05f))
-                {
-                    AddJumpForce();
-                    JumpAccum = 0;
-                    FallingTime = 0;
-                    IsFalling = true;
-                    IsInJumpMode = true;
-                    HasWallJumped = false;
-                    break;
-                }
+                AddJumpForce();
+                TimeBetweenJumpsAccum = 0;
+                FallingTime = 0;
+                IsInJumpMode = true;
+                HasWallJumped = false;
+                JumpAfterCollideCountdown = 0;
+                Debug.Log(DateTime.Now + ": Jump!");
+            }
+            else if(!HasWallJumped && WallJumpAfterCollideCountdown > 0)
+            {
+                AddJumpForce();
+                TimeBetweenJumpsAccum = 0;
+                FallingTime = 0;
+                IsInJumpMode = true;
+                HasWallJumped = true;
+                WallJumpAfterCollideCountdown = 0;
+                Debug.Log(DateTime.Now + ": Wall Jump!");
             }
         }
     }
@@ -177,12 +137,26 @@ public class PlayerController : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        foreach(ContactPoint contactPoint in collision.contacts)
+        CheckCollisionForJumpEligibility(collision);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        CheckCollisionForJumpEligibility(collision);
+    }
+
+    void CheckCollisionForJumpEligibility(Collision collision)
+    {
+        foreach (ContactPoint contactPoint in collision.contacts)
         {
-            if(contactPoint.normal.y > .1)
+            if (contactPoint.normal.y > .1)
             {
-                IsFalling = false;
-                Debug.Log("Not falling: " + contactPoint.normal.y);
+                JumpAfterCollideCountdown = JumpAfterCollideMax;
+                break;
+            }
+            else if(contactPoint.normal.y > -.2)
+            {
+                WallJumpAfterCollideCountdown = JumpAfterCollideMax;
             }
         }
     }
