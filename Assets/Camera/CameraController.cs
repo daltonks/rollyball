0c851746405a749e392382f876016ab2825dacad
daltonks <dalton_spillman@yahoo.com>
2016-09-27 22:40:08 -0500

    Camera is now manually rotated (with mouse for computer) Todo: implement touchscreen controls

--- Assets/Camera/CameraController.cs
+++ Assets/Camera/CameraController.cs
@@ -8,65 +8,46 @@ public class CameraController : MonoBehaviour
     public float CameraSpeed;
     public float DistanceToPlayer, LookOffsetY;
     public PlayerController Player { get; set; }
-    private Vector3 LastMousePos;
+    private float RotX, RotY;
 
     void Start()
     {
         StartingLoc = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         StartingRot = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
         Reset();
-        LastMousePos = Input.mousePosition;
+        Vector3 rot = transform.localRotation.eulerAngles;
+        RotX = rot.x;
+        RotY = rot.y;
     }
 
     public void Reset()
     {
         transform.position = StartingLoc - StartingRot * Vector3.forward;
-        //LerpToPlayer(1);
     }
 
-    void FixedUpdate()
-    {
-        if(Player == null)
-        {
-            return;
-        }
-        //LerpToPlayer(1);
-        /*
-        Quaternion quat = new Quaternion();
-        quat.SetLookRotation(Player.transform.position + Vector3.up * LookOffsetY + Player.RigidBody.velocity * LookPlayerVelocityMult - transform.position);
-        transform.rotation = Quaternion.Lerp(transform.rotation, quat, Time.fixedDeltaTime * LookLerpSpeed);*/
-    }
-
+    private const float ClampAngle = 80.0f;
     void LateUpdate()
     {
-        Vector3 mouseDif = Input.mousePosition - LastMousePos;
-        transform.RotateAround(Player.transform.position, Vector3.up, mouseDif.x);
-        transform.RotateAround(Player.transform.position, Vector3.left, mouseDif.y);
-        LastMousePos = Input.mousePosition;
-        transform.position = Player.transform.position + transform.rotation * Vector3.back * DistanceToPlayer;
-    }
-
-    /*
-    void LerpToPlayer(float lerpValue)
-    {
-        Vector3 newLocation = Player.transform.position + (transform.position - Player.transform.position).normalized * DistanceToPlayer;
-        newLocation.y = Player.transform.position.y + OffsetY;
-        Vector3 dif = newLocation - Player.transform.position;
-        RaycastHit[] raycastHits = Physics.RaycastAll(Player.transform.position, dif, dif.magnitude);
-        if(raycastHits.Length > 0)
+        RotX += -Input.GetAxis("Mouse Y") * CameraSpeed * Time.deltaTime;
+        RotY += Input.GetAxis("Mouse X") * CameraSpeed * Time.deltaTime;
+        if(RotY > 180)
         {
-            float minDistance = float.MaxValue;
-            Vector3 minPoint = newLocation;
-            foreach (RaycastHit hit in raycastHits)
-            {
-                if (hit.distance < minDistance)
-                {
-                    minDistance = hit.distance;
-                    minPoint = hit.point;
-                }
-            }
-            newLocation = minPoint;
+            RotY -= 360;
         }
-        transform.position = Vector3.Lerp(transform.position, newLocation, lerpValue);
-    }*/
+        if(RotY < -180)
+        {
+            RotY += 360;
+        }
+
+        RotX = Mathf.Clamp(RotX, -ClampAngle, ClampAngle);
+
+        transform.rotation = Quaternion.Euler(RotX, RotY, 0.0f);
+        Vector3 newPosition = Player.transform.position + transform.rotation * Vector3.back * DistanceToPlayer + Vector3.up * LookOffsetY;
+        RaycastHit hit;
+        if(Physics.Linecast(Player.transform.position, newPosition, out hit))
+        {
+            newPosition = hit.point;
+        }
+        transform.position = newPosition;
+    }
 }
